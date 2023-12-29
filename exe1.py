86e313e11143af69490fa14ebec67c833a1edbee
Yousef eslam <yousefeslam214@gmail.com>
2023-12-29 11:40:44 +0200

    done

--- exe1.py
+++ exe1.py
@@ -1,3 +1,6 @@
+# Write a python program to create a Vehicle class with 
+# max_speed and mileage instance attributes
+
 class Vehicle:
     def __init__(self,max_speed,mileage):
         self.max_speed = max_speed
@@ -5,3 +8,27 @@ class Vehicle:
 
 modelx = Vehicle(60, 10)
 print(modelx.max_speed, modelx.mileage)
+
+
+
+# create a child class bus that will inherit all of the
+# variables and methods of the Vehicle class
+
+class Bus(Vehicle):
+    pass
+
+school_bus = Bus(180,13)
+print(school_bus.mileage,school_bus.max_speed)
+
+
+
+# Define a property that must have the same value for
+# every class instance (object)
+
+class Car(Vehicle):
+    pass
+
+
+# check type of an object
+
+print(type(school_bus))
